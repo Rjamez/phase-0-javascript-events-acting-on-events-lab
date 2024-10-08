edd6ecdff5df5f694c2c1b089ac6cd2df3911bb9
Rjamez <robin.adhola.student@moringaschool.com>
2024-10-08 12:40:24 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+
+// moves the dodger left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+ function moveDodgerLeft() {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+//moves the dodger right
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+ 
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }else 
+    if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+  });
+
